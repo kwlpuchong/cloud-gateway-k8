8c14d3ae82e1837fe1345b6c04dd6a0f74bbd477
kwlpuchong <kwlpuchong@hotmail.com>
2021-08-27 17:01:30 +0800

    zuul

--- gateway/src/main/java/com/happyman/cloud/gateway/Application.java
+++ gateway/src/main/java/com/happyman/cloud/gateway/Application.java
@@ -3,22 +3,13 @@ package com.happyman.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableDiscoveryClient
+@EnableZuulProxy
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-            .route(p -> p
-            	.path("/alpha")
-            	.uri("http://alpha")).build();
-    }
 }
